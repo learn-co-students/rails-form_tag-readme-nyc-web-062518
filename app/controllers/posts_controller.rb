11d636fc1b741d82c91c2b17cf39151cdfab162d
Daniel Ran <danielr1735@gmail.com>
2018-07-21 13:48:45 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,17 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
+  def new
+  end
+
+  def create
+    @post = Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
 end
